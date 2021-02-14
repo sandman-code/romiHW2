38fc42a264385ae04556a1a1d1493dee1af584d6
Caden Crist <cade3217@gmail.com>
2021-02-13 19:47:36 -0500

    fixed

--- src/Chassis.cpp
+++ src/Chassis.cpp
@@ -4,40 +4,38 @@
  * Assume the robot drives about 12 inches / second* 
  * Take the number of inches, divide by 12 and drive that long 
  * 
- * */ 
+ * */
 
-void Chassis::driveDistance(float inches) 
-{     
+void Chassis::driveDistance(float inches)
+{
     encoders.getCountsAndResetLeft();
     encoders.getCountsAndResetRight();
 
-    while(encoders.getCountsRight() <  CPR * inches)
+    while (encoders.getCountsRight() < (inches / (wheelDiameter * PI)) * CPR)
     {
         int rightCount = encoders.getCountsRight();
         int leftCount = encoders.getCountsLeft();
         int err = rightCount - leftCount;
-        motors.setEfforts(100 - (err * errCoeff), 100 + (err * errCoeff));     
+        motors.setEfforts(100 - (err * errCoeff), 100 - (err * errCoeff));
     }
-    motors.setEfforts(0,0);
-
+    motors.setEfforts(0, 0);
 }
 
 /** 
  * * Assume the robot turns at about 180 degrees per second 
- * */ 
+ * */
 
-void Chassis::turnAngle(float degrees) 
-{     
+void Chassis::turnAngle(float degrees)
+{
     encoders.getCountsAndResetLeft();
     encoders.getCountsAndResetRight();
 
-    while (encoders.getCountsLeft() < ((CPR / 360) * degrees))
+    while (encoders.getCountsLeft() < (((wheelTrack * PI * degrees) / 360) / (wheelDiameter * PI)) * CPR)
     {
         int rightCount = encoders.getCountsRight();
         int leftCount = encoders.getCountsLeft();
         int err = rightCount - leftCount;
-        motors.setEfforts(100 - (err * errCoeff), -100 + (err * errCoeff)); 
+        motors.setEfforts(100 - (err * errCoeff), -100 - (err * errCoeff));
     }
-    motors.setEfforts(0,0);
-         
-} 
+    motors.setEfforts(0, 0);
+}
